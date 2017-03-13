c3b328c6307fc151696e687677e969f669a5bc6f
victorjambo <victorjambo@live.com>
2017-03-13 19:27:02 +0300

    CRUD n Validation

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -1,24 +1,45 @@
 class ArticlesController < ApplicationController
 	def new
+		@article = Article.new
 	end
 
 	def index
 		@articles = Article.all
 	end
 
+	def edit
+		@article = find_params
+	end
+
 	def show
-		@article =  Article.find(params[:id])
+		@article =  find_params
 	end
 
 	def create
 		@article = Article.new(article_params)
 
-		@article.save
-		redirect_to @article
+		if @article.save
+			redirect_to @article
+		else
+			render 'new'
+		end
+	end
+
+	def update
+		@article = find_params
+
+		if @article.update(article_params)
+			redirect_to @article
+		else
+			render 'edit'
+		end
 	end
 
 	private
 	def article_params
 		params.require(:article).permit(:title, :text)
+	end
+	def find_params
+		Article.find(params[:id])
 	end
 end
